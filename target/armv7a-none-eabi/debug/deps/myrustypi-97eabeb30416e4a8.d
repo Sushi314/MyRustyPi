e27c92e6b5070804c321780dcbaf27445799691f
Sushi <98758109+Sushi314@users.noreply.github.com>
2022-11-19 17:11:59 -0500

    Add files via upload

---
+++ target/armv7a-none-eabi/debug/deps/myrustypi-97eabeb30416e4a8.d
@@ -0,0 +1,5 @@
+C:\Users\Sushi\Documents\myRustyPiZero\myrustypi\target\armv7a-none-eabi\debug\deps\myrustypi-97eabeb30416e4a8: src\main.rs
+
+C:\Users\Sushi\Documents\myRustyPiZero\myrustypi\target\armv7a-none-eabi\debug\deps\myrustypi-97eabeb30416e4a8.d: src\main.rs
+
+src\main.rs:
---
+++ target/armv7a-none-eabi/debug/myrustypi.d
@@ -0,0 +1,1 @@
+C:\Users\Sushi\Documents\myRustyPiZero\myrustypi\target\armv7a-none-eabi\debug\myrustypi: C:\Users\Sushi\Documents\myRustyPiZero\myrustypi\src\main.rs
